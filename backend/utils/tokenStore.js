9c3ab87e15523a152ba2769e20755f0b27175c6a
MelanX <info@melanx.de>
2026-03-19 15:56:53 +0100

    Fix token store: purge expired rows and use async/await

--- backend/utils/tokenStore.js
+++ backend/utils/tokenStore.js
@@ -1,5 +1,7 @@
 const { DATABASE_FILE } = require("./fileService");
 const sqlite3 = require('sqlite3').verbose();
+const { promisify } = require('util');
+
 const db = new sqlite3.Database(DATABASE_FILE);
 
 db.exec(`
@@ -10,18 +12,25 @@ db.exec(`
     );
 `);
 
-function revoke(jti, expires) {
-    return new Promise(res => {
-        db.run(
-            'INSERT OR REPLACE INTO revoked_tokens VALUES (?, ?)', [ jti, expires ], res
-        )
-    });
+const run = promisify(db.run.bind(db));
+const get = promisify(db.get.bind(db));
+
+async function revoke(jti, expires) {
+    await run('INSERT OR REPLACE INTO revoked_tokens VALUES (?, ?)', [jti, expires]);
 }
 
-function isRevoked(jti) {
-    return new Promise(res => {
-        db.get('SELECT 1 FROM revoked_tokens WHERE jti = ?', [ jti ], (err, row) => res(!!row))
-    });
+async function isRevoked(jti) {
+    const row = await get('SELECT 1 FROM revoked_tokens WHERE jti = ?', [jti]);
+    return !!row;
 }
+
+async function purgeExpired() {
+    await run('DELETE FROM revoked_tokens WHERE expires < ?', [Date.now()]);
+}
+
+// Purge on startup and every 24 hours.
+// .unref() prevents the interval from keeping the process alive during shutdown.
+purgeExpired();
+setInterval(purgeExpired, 24 * 60 * 60 * 1000).unref();
 
 module.exports = { revoke, isRevoked };
